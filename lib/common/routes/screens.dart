f8206ed58038599587d43d5b3cd1dc9399e84271
muteeurrehman <mutee.ur.rehman786@gmail.com>
2024-04-28 13:04:21 +0500

    makes welcome library for easy imports

--- lib/common/routes/screens.dart
+++ lib/common/routes/screens.dart
@@ -1,7 +1,6 @@
 import 'package:chat_using_firebase/common/routes/app_routes.dart';
-import 'package:chat_using_firebase/screens/weclome/binding.dart';
-import 'package:chat_using_firebase/screens/weclome/view.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:chat_using_firebase/screens/weclome/index.dart';
+import 'package:get/get.dart';
 
 class AppScreens {
   static const initial = AppRoutes.initial;
